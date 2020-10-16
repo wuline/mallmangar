0997dd3fc2f2ea71be85e49ce7dfbfe606751dd5
wuline <714878605@qq.com>
2020-10-16 15:26:34 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+//使用vue插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+/* new vue({
+  router,
+  components:{App},
+  template:'<App/>'
+}) */
